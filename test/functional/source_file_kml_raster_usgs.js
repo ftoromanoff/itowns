67dd77df6ace13ff144586a08b5da31c7e663d97
ftoromanoff <francois.toromanoff@ign.fr>
2023-04-03 16:37:15 +0200

    test(examples): add new functional tests

--- test/functional/source_file_kml_raster_usgs.js
+++ test/functional/source_file_kml_raster_usgs.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 
-describe('source_file_geojson_raster', function _() {
+describe('source_file_kml_raster_usgs', function _() {
     let result;
     before(async () => {
-        result = await loadExample('examples/source_file_geojson_raster.html', this.fullTitle());
+        result = await loadExample('examples/source_file_kml_raster_usgs.html', this.fullTitle());
     });
 
     it('should run', async () => {
@@ -20,25 +20,25 @@ describe('source_file_geojson_raster', function _() {
 
             return Promise.all(promises);
         });
-        assert.equal(2, features.length);
+        assert.equal(features.length, 2); // the layer and the LabelLayer
+        assert.equal(features[0].uuid, features[1].uuid);
     });
 
     it('should pick feature from Layer with SourceFile', async () => {
-        const pickedFeatures = await page.evaluate(() => {
-            const precision = view.getPixelsToDegrees(5);
-            const geoCoord = new itowns.Coordinates('EPSG:4326', 1.41955, 42.88613, 0);
+        const features = await page.evaluate(() => {
             const promises = [];
             const layers = view.getLayers(l => l.source && l.source.isFileSource);
             for (let i = 0; i < layers.length; i++) {
                 promises.push(layers[i].source.loadData({}, { crs: 'EPSG:4326', buildExtent: false }));
             }
 
-            return Promise.all(promises).then(fa => fa.filter(f => itowns.FeaturesUtils
-                .filterFeaturesUnderCoordinate(geoCoord, f, precision).length));
+            return Promise.all(promises);
         });
-        assert.equal(1, pickedFeatures.length);
-        assert.equal(1, pickedFeatures[0].features.length);
-        assert.equal(pickedFeatures[0].features[0].type, 2);
-        assert.equal(pickedFeatures[0].features[0].geometries[0].properties.nom, 'Ariège');
+
+        assert.equal(2, features.length);
+        assert.equal(features[0].uuid, features[1].uuid);
+
+        assert.equal(features[0].features.length, 1);
+        assert.equal(features[0].features[0].type, 0);
     });
 });
