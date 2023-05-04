630ccb4200567ad49026aef861ecb42f3094fa2f
ftoromanoff <francois.toromanoff@ign.fr>
2023-05-04 14:04:28 +0200

    refactor(test): message if error

--- test/unit/feature2mesh.js
+++ test/unit/feature2mesh.js
@@ -58,40 +58,43 @@ describe('Feature2Mesh', function () {
     const parsed3 = GeoJsonParser.parse(geojson3, { in: { crs: 'EPSG:3946' }, out: { crs: 'EPSG:3946', buildExtent: true, mergeFeatures: false, structure: '3d' } });
 
     it('rect mesh area should match geometry extent', function (done) {
-        parsed.then((collection) => {
-            const mesh = Feature2Mesh.convert()(collection).meshes;
-            const extentSize = collection.extent.planarDimensions();
+        parsed
+            .then((collection) => {
+                const mesh = Feature2Mesh.convert()(collection).meshes;
+                const extentSize = collection.extent.planarDimensions();
 
-            assert.equal(
-                extentSize.x * extentSize.y,
-                computeAreaOfMesh(mesh.children[0]));
-            done();
-        }).catch(done);
+                assert.equal(
+                    extentSize.x * extentSize.y,
+                    computeAreaOfMesh(mesh.children[0]));
+                done();
+            }).catch(done);
     });
 
     it('square mesh area should match geometry extent minus holes', function (done) {
-        parsed.then((collection) => {
-            const mesh = Feature2Mesh.convert()(collection).meshes;
+        parsed
+            .then((collection) => {
+                const mesh = Feature2Mesh.convert()(collection).meshes;
 
-            const noHoleArea = computeAreaOfMesh(mesh.children[0]);
-            const holeArea = computeAreaOfMesh(mesh.children[1]);
-            const meshWithHoleArea = computeAreaOfMesh(mesh.children[2]);
+                const noHoleArea = computeAreaOfMesh(mesh.children[0]);
+                const holeArea = computeAreaOfMesh(mesh.children[1]);
+                const meshWithHoleArea = computeAreaOfMesh(mesh.children[2]);
 
-            assert.equal(
-                noHoleArea - holeArea,
-                meshWithHoleArea);
-            done();
-        }).catch(done);
+                assert.equal(
+                    noHoleArea - holeArea, meshWithHoleArea,
+                );
+                done();
+            }).catch(done);
     });
 
     it('convert points, lines and mesh', function (done) {
-        parsed2.then((collection) => {
-            const mesh = Feature2Mesh.convert()(collection).meshes;
-            assert.equal(mesh.children[0].type, 'Points');
-            assert.equal(mesh.children[1].type, 'Line');
-            assert.equal(mesh.children[2].type, 'Mesh');
-            done();
-        }).catch(done);
+        parsed2
+            .then((collection) => {
+                const mesh = Feature2Mesh.convert()(collection).meshes;
+                assert.equal(mesh.children[0].type, 'Points');
+                assert.equal(mesh.children[1].type, 'Line');
+                assert.equal(mesh.children[2].type, 'Mesh');
+                done();
+            }).catch(done);
     });
 
     it('convert to instanced meshes', function (done) {
