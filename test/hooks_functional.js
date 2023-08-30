acd686809f266d99e099032512aa25f2b3b1fc1b
ftoromanoff <francois.toromanoff@ign.fr>
2023-08-30 12:36:46 +0200

    fix(test-functional): augment npm run test-functional timeout and corrections on hooks

--- test/hooks_functional.js
+++ test/hooks_functional.js
@@ -115,7 +115,7 @@ const loadExample = async (url, screenshotName) => {
 
     pageErrors.forEach((e) => { throw e; });
 
-    await page.waitForFunction(() => typeof (view) === 'object');
+    await page.waitForFunction(() => typeof view === 'object' && view instanceof itowns.View);
 
     await page.evaluate(() => {
         itowns.CameraUtils.defaultStopPlaceOnGroundAtEnd = true;
@@ -129,6 +129,8 @@ const loadExample = async (url, screenshotName) => {
                 itowns.CameraUtils.stop(view, view.camera3D);
             });
             await layersAreInitialized();
+        } else {
+            throw e;
         }
     }
 
@@ -203,7 +205,7 @@ exports.mochaHooks = {
         });
 
         // the page all tests will be tested in
-        return browser.newPage().then((p) => { global.page = p; });
+        global.page = await browser.newPage();
     },
     // store initial position for restoration after the test
     afterAll(done) {
